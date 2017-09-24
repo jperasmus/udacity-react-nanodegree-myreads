77af9a01b1ea8ab19b152c2f5f674351e56c8469
JP Erasmus <jperasmus11@gmail.com>
2017-09-24 21:13:58 +0200

    feat: Improve search functionality
    * Ensure that the books search is debounced so it doesn't make a tonne of unnecessary network requests.
    * Display better meta data for the results
    * Autofocus the search input for easier searching

--- src/Components/SearchBooks.js
+++ src/Components/SearchBooks.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Books from './Books';
-import { search } from '../BooksAPI';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import debounce from "lodash.debounce";
+import Books from "./Books";
+import { search } from "../BooksAPI";
 
 class SearchBooks extends Component {
   constructor(props) {
@@ -9,38 +10,61 @@ class SearchBooks extends Component {
 
     this.state = {
       results: [],
+      count: 0,
+      keyword: "",
       searching: false,
       searchFailed: false
     };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleInputChange(e) {
-    const keyword = e.target.value;
+  handleInputChange = debounce(keyword => {
+    // Not handling the event where the keyword is empty because we're defaulting to an empty array below
+
     this.setState({
       searching: true,
-      searchFailed: false
+      searchFailed: false,
+      keyword
     });
 
-    search(keyword, 200)
-      .then(results => {
-        this.setState({ results, searching: false });
+    search(keyword, 10) // NOTE: The search maxResults property doesn't actually do anything, always defaults to 20
+      .then(response => {
+        const results = Array.isArray(response) ? response : [];
+        const count = results.length;
+        this.setState({ results, count, searching: false });
       })
       .catch(() => {
-        this.setState({ results: [], searching: false, searchFailed: true });
+        this.setState({ results: [], count: 0, searching: false, searchFailed: true });
       });
-  }
+  }, 250);
 
   render() {
     let Results;
 
     if (this.state.searchFailed) {
-      Results = () => <div>Searching failed :(</div>;
+      Results = () => (
+        <div className="search-books-results-meta">
+          Searching for <em>"{this.state.keyword}"</em> failed :(
+        </div>
+      );
     } else if (this.state.searching) {
-      Results = () => <div>Finding some books quick...</div>;
+      Results = () => (
+        <div className="search-books-results-meta">
+          Searching for books matching <em>"{this.state.keyword}"</em>
+        </div>
+      );
     } else {
-      Results = () => <Books books={this.state.results} moveBook={this.props.moveBook} />;
+      Results = () => {
+        return this.state.keyword && this.state.count ? (
+          <div>
+            <div className="search-books-results-meta">
+              Displaying {this.state.count} books for <em>"{this.state.keyword}"</em>
+            </div>
+            <Books books={this.state.results} moveBook={this.props.moveBook} bookAction="add" />
+          </div>
+        ) : (
+          <div className="search-books-results-meta">No books to display</div>
+        );
+      };
     }
 
     return (
@@ -61,7 +85,8 @@ class SearchBooks extends Component {
             <input
               type="text"
               placeholder="Search by title or author"
-              onChange={this.handleInputChange}
+              onChange={({ target: { value } }) => this.handleInputChange(value)}
+              autoFocus
             />
           </div>
         </div>
