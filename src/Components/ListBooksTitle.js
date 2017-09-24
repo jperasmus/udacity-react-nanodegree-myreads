89c312ad2aa0f2d87d69951ba731cb288d8b50bc
JP Erasmus <jperasmus11@gmail.com>
2017-09-24 17:13:19 +0200

    feat: Add `ListBooksTitle` component

---
+++ src/Components/ListBooksTitle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ListBooksTitle = props => {
+  return (
+    <div className="list-books-title">
+      <h1>{props.title}</h1>
+    </div>
+  );
+};
+
+ListBooksTitle.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+export default ListBooksTitle;
