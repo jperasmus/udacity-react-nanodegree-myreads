e399bffe86f0ca8fce74952cb1937c813809803b
JP Erasmus <jperasmus11@gmail.com>
2017-09-24 20:05:06 +0200

    feat: Move main books state to App component
    This allows for the same state to be easier shared between the List and the Search

--- src/App.js
+++ src/App.js
@@ -2,14 +2,89 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import SearchBooks from './Components/SearchBooks';
 import ListBooks from './Components/ListBooks';
+import * as BooksAPI from './BooksAPI';
 import './App.css';
 
 class BooksApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentlyReading: [],
+      wantToRead: [],
+      read: [],
+      fetchingBooks: true,
+      fetchingBooksFailed: false
+    };
+
+    this.moveBook = this.moveBook.bind(this);
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then(books => {
+        // TODO: For a production app, refactor this so that each section does not need to iterate through all the books with the "filter"
+        this.setState({
+          currentlyReading: books.filter(book => book.shelf === 'currentlyReading'),
+          wantToRead: books.filter(book => book.shelf === 'wantToRead'),
+          read: books.filter(book => book.shelf === 'read'),
+          fetchingBooks: false
+        });
+      })
+      .catch(() => {
+        this.setState({
+          fetchingBooksFailed: true
+        });
+      });
+  }
+
+  /**
+   * @description Move a book from one shelf to another.
+   * @param {object} book
+   * @param {string} oldShelf
+   * @param {string} newShelf
+   */
+  moveBook(book, oldShelf, newShelf) {
+    this.setState(state => {
+      const updatedState = {
+        [newShelf]: state[newShelf].concat([{ ...book, ...{ shelf: newShelf } }])
+      };
+
+      // When adding new books, they don't have an existing shelf
+      if (oldShelf) {
+        updatedState[oldShelf] = state[oldShelf].filter(shelfBook => shelfBook.id !== book.id);
+      }
+
+      return updatedState;
+    });
+
+    BooksAPI.update(book, newShelf)
+      .then(() => {}) // All good, we've already optimistically moved the book to its new shelf
+      .catch(() => {
+        // The update failed, we need to move the book back to its previous shelf
+        this.setState(state => {
+          const updatedState = {
+            [oldShelf]: state[oldShelf].concat([{ ...book, ...{ shelf: oldShelf } }])
+          };
+
+          if (newShelf) {
+            updatedState[newShelf] = state[newShelf].filter(shelfBook => shelfBook.id !== book.id);
+          }
+
+          return updatedState;
+        });
+      });
+  }
+
   render() {
     return (
       <div className="app">
-        <Route path="/search" component={SearchBooks} />
-        <Route exact path="/" component={ListBooks} />
+        <Route
+          exact
+          path="/"
+          render={() => <ListBooks moveBook={this.moveBook} {...this.state} />}
+        />
+        <Route path="/search" render={() => <SearchBooks moveBook={this.moveBook} />} />
       </div>
     );
   }
--- src/Components/ListBooks.js
+++ src/Components/ListBooks.js
@@ -2,85 +2,31 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ListBooksTitle from './ListBooksTitle';
 import Bookshelf from './Bookshelf';
-import * as BooksAPI from '../BooksAPI';
 
 class ListBooks extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentlyReading: [],
-      wantToRead: [],
-      read: [],
-      fetchingBooks: true,
-      fetchingBooksFailed: false
-    };
-
-    this.moveBook = this.moveBook.bind(this);
-  }
-
-  componentDidMount() {
-    BooksAPI.getAll()
-      .then(books => {
-        // TODO: For a production app, refactor this so that each section does not need to iterate through all the books with the "filter"
-        this.setState({
-          currentlyReading: books.filter(book => book.shelf === 'currentlyReading'),
-          wantToRead: books.filter(book => book.shelf === 'wantToRead'),
-          read: books.filter(book => book.shelf === 'read'),
-          fetchingBooks: false
-        });
-      })
-      .catch(() => {
-        this.setState({
-          fetchingBooksFailed: true
-        });
-      });
-  }
-
-  /**
-   * @description Move a book from one shelf to another.
-   * @param {object} book
-   * @param {string} oldShelf
-   * @param {string} newShelf
-   */
-  moveBook(book, oldShelf, newShelf) {
-    this.setState(state => ({
-      [oldShelf]: state[oldShelf].filter(shelfBook => shelfBook.id !== book.id),
-      [newShelf]: state[newShelf].concat([{ ...book, ...{ shelf: newShelf } }])
-    }));
-
-    BooksAPI.update(book, newShelf)
-      .then(() => {
-        // All good, we've already optimistically moved the book to its new shelf
-      })
-      .catch(() => {
-        // The update failed, we need to move the book back to its previous shelf
-        this.setState(state => ({
-          [newShelf]: state[newShelf].filter(shelfBook => shelfBook.id !== book.id),
-          [oldShelf]: state[oldShelf].concat([{ ...book, ...{ shelf: oldShelf } }])
-        }));
-      });
-  }
-
   render() {
-    let Content = null;
+    const {
+      fetchingBooksFailed,
+      fetchingBooks,
+      currentlyReading,
+      wantToRead,
+      read,
+      moveBook
+    } = this.props;
+    let Content;
 
-    if (this.state.fetchingBooksFailed) {
+    if (fetchingBooksFailed) {
       Content = () => <div className="list-books-content-failed">Fetching the books failed...</div>;
-    } else if (this.state.fetchingBooks) {
+    } else if (fetchingBooks) {
       Content = () => (
         <div className="list-books-content-loading">One second, quickly fetching the books...</div>
       );
     } else {
       Content = () => (
         <div>
-          <Bookshelf
-            title="Currently Reading"
-            books={this.state.currentlyReading}
-            moveBook={this.moveBook}
-          />
-          <Bookshelf title="Want to Read" books={this.state.wantToRead} moveBook={this.moveBook} />
-          <Bookshelf title="Read" books={this.state.read} moveBook={this.moveBook} />
+          <Bookshelf title="Currently Reading" books={currentlyReading} moveBook={moveBook} />
+          <Bookshelf title="Want to Read" books={wantToRead} moveBook={moveBook} />
+          <Bookshelf title="Read" books={read} moveBook={moveBook} />
         </div>
       );
     }
